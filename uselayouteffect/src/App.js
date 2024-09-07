dd0c86ef9c48d0b7d4a56f9b66368266cd01caf7
Debalina99 <debalinalaha13630@gmail.com>
2024-09-07 23:06:11 +0530

    Initial commit

---
+++ uselayouteffect/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import UseLayoutEffect from './useLayoutEffect';
+
+function App() {
+  return (
+    <div className="App">
+     <UseLayoutEffect></UseLayoutEffect>
+    </div>
+  );
+}
+
+export default App;
---
+++ uselayouteffect/src/useLayoutEffect.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState, useLayoutEffect } from 'react';
+
+export default function UseLayoutEffect() {
+    const [counter, setCounter] = useState(0)
+
+    useEffect(() => {
+        console.log("useEffect With Empty Dependency");
+    })
+    useEffect(() => {
+        console.log("useEffect Dependency with values");
+    }, [counter])
+
+    useLayoutEffect(() => {
+        console.log("useLayoutEffect With Empty Dependency");
+    })
+    useLayoutEffect(() => {
+        console.log("useLayoutEffect Dependency with values");
+    }, [counter])
+    
+    const clickHandler = () => {
+        setCounter((prev) => prev + 1)
+    }
+    return (
+        <div>
+            <h3>useLayoutEffect Example</h3>
+            <h4>{counter}</h4>
+            <button onClick={clickHandler}>Click</button>
+        </div>
+    )
+}
